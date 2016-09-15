fa5d890c42c90d862df8179656d1f54e7461fce3
vanuvanu <vanamali.vanu@gmail.com>
2016-09-15 07:12:36 +0530

    Created by Coding Ground Client

---
+++ scalaPractice/pointDemo.scala
@@ -0,0 +1,18 @@
+import java.io._
+class Point(val xc:Int, val yc:Int){
+    var x:Int=xc
+    var y:Int=yc
+  def move(dx:Int, dy:Int){
+      x=x+dx
+      y=y+dy
+      println ("Point x location : " + x);
+      println ("Point y location : " + y);
+  }  
+}
+object pointDemo{
+    def main(args: Array[String]){
+   val pt= new Point(10,10)
+   //move to new location
+   pt.move(10,20)
+}
+}
