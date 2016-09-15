13f38d61c2429d92c8c2387ae2367fc743ce70de
vanuvanu <vanamali.vanu@gmail.com>
2016-09-15 05:54:12 +0530

    Created by Coding Ground Client

---
+++ New Project-20160915/Demo.scala
@@ -0,0 +1,10 @@
+object Demo{
+def main(args: Array[String]){
+var myVar1: Int=10;
+val myVal1: String="scala with datatype declaration"
+var myVar2 = 20;
+val myVal2="scala without datatype declaration"
+println(myVal1);println(myVar1);println(myVal2)
+println(myVal2);
+}
+}
